5eb01c2b6596691216287f42bf55bbcf2e77fb34
Yehor <139066654+Yehor-Kovalenko@users.noreply.github.com>
2026-09-18 20:33:13 +0200

    Configure custom marker icons for Leaflet

--- src/layout/panels/MapPanel.tsx
+++ src/layout/panels/MapPanel.tsx
@@ -2,9 +2,20 @@ import { useEffect, useRef, useState } from 'react';
 import L from 'leaflet';
 import mapImage from '@/assets/map.png'
 import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import './MapPanel.css';
 import { createGeoTransformer, type ControlPoint } from '@/service/geoTransformation';
 import { ANIMALS_LIST } from '@/service/animals';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const MapPanel = () => {
   const mapRef = useRef<HTMLDivElement>(null);
